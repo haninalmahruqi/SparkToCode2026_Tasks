df654e30a2278416504b3e419d28097113a21703
haninalmahruqi <haninalmahruqi26@gmail.com>
2026-07-06 01:21:40 +0400

    solve problem1 (Countdown Timer)

--- SolveTask2/Program.cs
+++ SolveTask2/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            //1- Countdown Timer
+            // Some Code To Solove The Task
+
+            Console.WriteLine("Enter the countdown time: ");
+            int countdownTime = int.Parse(Console.ReadLine());
+
+            for (int i = countdownTime; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(1000);
+
+
+            }
+            Console.WriteLine("Countdown finished!");
+
+            ///////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
