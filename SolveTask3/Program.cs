c2b400b3986449d475a0a25555ebc93c99ce6a7a
haninalmahruqi <haninalmahruqi26@gmail.com>
2026-07-06 00:40:35 +0400

    Solve problem1 (Absolute Difference)

--- SolveTask3/Program.cs
+++ SolveTask3/Program.cs
@@ -4,7 +4,76 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            ////Task 1 – Absolute Difference
+
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double difference = Math.Abs(num1 - num2);
+
+            Console.WriteLine("Positive Difference: " + difference);
+
+
+
+            ///////////////////////////////////////////////////////////////
+            ///
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
