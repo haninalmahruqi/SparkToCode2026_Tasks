9056e4e47b31dc6c42fb8d2ceb74b7c4127f4195
haninalmahruqi <haninalmahruqi26@gmail.com>
2026-07-06 01:24:11 +0400

    solve problem4 (Password Retry)

--- SolveTask2/Program.cs
+++ SolveTask2/Program.cs
@@ -56,6 +56,24 @@
 
 
 
+            //4- Password Retry 
+            // Some Code To Solove The Task
+
+            string password = "1234";
+
+            Console.WriteLine("Enter the password: ");
+            string inputpassword = Console.ReadLine();
+
+            while (inputpassword != password)
+            {
+                Console.WriteLine("Incorrect password. Please try again: ");
+                Console.WriteLine("Enter the password: ");
+                inputpassword = Console.ReadLine();
+            }
+
+            Console.WriteLine("Access granted!");
+
+            ////////////////////////////////////////////////////////////////
 
 
 
