ce70a4bbe15529ca22e276e9e106ef4fce329452
haninalmahruqi <haninalmahruqi26@gmail.com>
2026-07-06 02:06:38 +0400

    solve problem1 (personal info card)

--- Task1Solution/Program.cs
+++ Task1Solution/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //problem1:Personal Info Card
+            String name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Hight: " + height);
+            Console.WriteLine("Student: " + isStudent);
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
