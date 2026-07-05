88abfe9a53e9f54ed985d1bb42f1d6d72a999bac
haninalmahruqi <haninalmahruqi26@gmail.com>
2026-07-06 00:52:03 +0400

    solve problem 12(Birthday Insights )

--- SolveTask3/Program.cs
+++ SolveTask3/Program.cs
@@ -211,6 +211,35 @@
             ///////////////////////////////////////////////////////////////////////////////
             ///
 
+            ///Task 12 – Birthday Insights 
+            ///
+
+
+            try
+            {
+                Console.Write("Enter date of birth (yyyy-MM-dd): ");
+                DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+                int age = DateTime.Today.Year - birthDate.Year;
+
+                if (DateTime.Today.Month < birthDate.Month ||
+                    (DateTime.Today.Month == birthDate.Month &&
+                     DateTime.Today.Day < birthDate.Day))
+                {
+                    age--;
+                }
+
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine("Born on: " + birthDate.DayOfWeek);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid date.");
+            }
+
+            //////////////////////////////////////////////////////////////////////////////
+            ///
+
 
 
 
