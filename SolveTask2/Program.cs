a735273bd9188284b3d812e8d5a30e98e0cc5904
haninalmahruqi <haninalmahruqi26@gmail.com>
2026-07-06 01:22:30 +0400

    solve problem2 (Sum Of Nambers 1 to N)

--- SolveTask2/Program.cs
+++ SolveTask2/Program.cs
@@ -25,6 +25,21 @@
 
 
 
+            //2- Sum Of Nambers 1 to N
+            // Some Code To Solove The Task
+
+            Console.WriteLine("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                total += i;
+            }
+            Console.WriteLine("The final sum is:" + total);
+
+            //////////////////////////////////////////////////////
+
 
 
 
